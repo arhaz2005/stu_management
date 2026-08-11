98119f5d99fab693c7f2ba6fd6ffe7d8ac0ca402
safarnejad <@arhazsafar@gmail.com>
2026-08-11 15:14:26 +0330

    add initial list

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿StudentManager manager = new StudentManager();
+bool flag = true;
+while (flag)
+{
+    Console.WriteLine("---------------------------------------------------");
+    Console.WriteLine("1-Add student");
+    Console.WriteLine("2-delete student by id");
+    Console.WriteLine("3-Edit");
+    Console.WriteLine("4-show the list");
+    Console.WriteLine("5-search by name");
+    Console.WriteLine("6-search by id");
+    Console.WriteLine("7-order by age");
+    Console.WriteLine("8-exist");
+}
